aac502474aec4d924621d06ff084e0b42eab1549
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-09-13 18:14:07 +0300

    refactor(user): update permissions logic for user update

--- backend/src/middlewares/permissions/permissions_services.ts
+++ backend/src/middlewares/permissions/permissions_services.ts
@@ -16,6 +16,12 @@ import { AssociatedDataError, PermissionError } from "./permission_errors";
     * Admin: All roles except "Admin"
     * Managers: employees only
     * Employees: None
+
+ * UPDATE:
+------------
+    * Admin: All roles except his roles and active status
+    * Managers: employees active status and his username and password
+    * Employees: his username and password only
  */
 
 class UserPermissionsService {
@@ -36,34 +42,38 @@ class UserPermissionsService {
         return !this.isAdmin(user) && !this.isManager(user);
     }
 
+    private static isAccountOwner(
+        requestingUser: UserWithId,
+        targetUser: UserWithId
+    ): boolean {
+        return requestingUser._id.toString() === targetUser._id.toString();
+    }
+
     // Helper method to validate users and throw errors if invalid
     private static validateUsers(
         requestingUser: UserWithId,
-        targetUser: UserWithId | UserRequestBody,
-        action: "delete" | "create"
+        targetUser: UserWithId | UserRequestBody
     ): void {
         if (!targetUser || !requestingUser) {
             throw new PermissionError("Access Denied", 403);
         }
 
-        if (action === "delete" || action === "create") {
-            if (this.isEmployee(requestingUser)) {
-                throw new PermissionError(
-                    "You don't have the permissions to perform this action",
-                    401
-                );
-            }
+        if (this.isEmployee(requestingUser)) {
+            throw new PermissionError(
+                "You don't have the permissions to perform this action",
+                401
+            );
+        }
 
-            if (this.isAdmin(targetUser)) {
-                throw new PermissionError("This action is not permitted.", 401);
-            }
+        if (this.isAdmin(targetUser)) {
+            throw new PermissionError("This action is not permitted.", 401);
+        }
 
-            if (this.isManager(targetUser) && !this.isAdmin(requestingUser)) {
-                throw new PermissionError(
-                    "You don't have the permissions to perform this action",
-                    401
-                );
-            }
+        if (this.isManager(targetUser) && !this.isAdmin(requestingUser)) {
+            throw new PermissionError(
+                "You don't have the permissions to perform this action",
+                401
+            );
         }
     }
 
@@ -71,7 +81,7 @@ class UserPermissionsService {
         requestingUser: UserWithId,
         targetUser: UserWithId
     ): Promise<void> {
-        this.validateUsers(requestingUser, targetUser, "delete");
+        this.validateUsers(requestingUser, targetUser);
 
         // Check for associated tasks
         const associatedNotes = await NoteModel.find({
@@ -88,7 +98,95 @@ class UserPermissionsService {
         requestingUser: UserWithId,
         targetUser: UserRequestBody
     ): Promise<void> {
-        this.validateUsers(requestingUser, targetUser, "create");
+        this.validateUsers(requestingUser, targetUser);
+    }
+
+    static async canUpdateUser(
+        requestingUser: UserWithId,
+        targetUser: UserWithId,
+        updates: UserRequestBody
+    ): Promise<void> {
+        const {
+            roles: providedRoles,
+            active: providedActiveStatus,
+            username: providedUsername,
+            password: providedPassword,
+        } = updates;
+
+        // User attempts to modify user roles to be 'admin'
+        if (providedRoles?.includes("admin")) {
+            throw new PermissionError(
+                "You do not have permission to perform this action.",
+                401
+            );
+        }
+
+        // only admin is able to modify users role
+        if (!this.isAdmin(requestingUser) && providedRoles) {
+            throw new PermissionError(
+                "You do not have permission to perform this action.",
+                401
+            );
+        }
+
+        if (this.isAdmin(targetUser)) {
+            if (
+                providedRoles ||
+                providedActiveStatus === false ||
+                !this.isAdmin(requestingUser)
+            ) {
+                throw new PermissionError(
+                    "You do not have permission to perform this action.",
+                    401
+                );
+            }
+        }
+
+        // Employees can't modify another user account
+        if (this.isEmployee(requestingUser)) {
+            if (!this.isAccountOwner(requestingUser, targetUser)) {
+                throw new PermissionError(
+                    "You do not have permission to perform this action.",
+                    401
+                );
+            }
+        }
+
+        // Managers can't modify another manager account
+        if (this.isManager(requestingUser)) {
+            if (
+                this.isManager(targetUser) &&
+                !this.isAccountOwner(requestingUser, targetUser)
+            ) {
+                throw new PermissionError(
+                    "You do not have permission to perform this action.",
+                    401
+                );
+            }
+        }
+
+        // User can't change his own active status
+        if (
+            this.isAccountOwner(requestingUser, targetUser) &&
+            providedActiveStatus !== undefined
+        ) {
+            throw new PermissionError(
+                "You do not have permission to perform this action.",
+                401
+            );
+        }
+
+        // Only Admin or account owner can change username and password
+        if (
+            !this.isAdmin(requestingUser) &&
+            !this.isAccountOwner(requestingUser, targetUser) &&
+            (providedUsername || providedPassword)
+        ) {
+            throw new PermissionError(
+                "You do not have permission to perform this action.",
+                401
+            );
+        }
     }
 }
 
