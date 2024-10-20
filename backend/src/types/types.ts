aea461f61f720939cea84e0a773cb6e2c9684871
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-10-20 18:06:21 +0300

    fix: type checking bug

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -3,51 +3,51 @@ import { Document, SortOrder, Types } from "mongoose";
 export type Role = "employee" | "manager" | "admin";
 
 export interface User {
-    username: string;
-    password: string;
-    roles: Role[];
-    active: boolean;
+	username: string;
+	password: string;
+	roles: Role[];
+	active: boolean;
 }
 
 export interface UserWithId extends User, Document {
-    _id: Types.ObjectId;
+	_id: Types.ObjectId;
 }
 
 export interface UserRequestBody extends Partial<User> {
-    id: string;
+	id: string;
 }
 
 export interface UserQueryResponse {
-    data: UserWithId[];
-    totalPages: number;
+	data: UserWithId[];
+	totalPages: number;
 }
 
 export interface Note {
-    user: Types.ObjectId;
-    title: string;
-    text: string;
-    completed: boolean;
-    ticket: number;
+	user: Types.ObjectId | UserWithId;
+	title: string;
+	text: string;
+	completed: boolean;
+	ticket: number;
 }
 
 export interface NoteWithId extends Note, Document {
-    _id: Types.ObjectId;
+	_id: Types.ObjectId;
 }
 
 export interface PopulatedNote extends Omit<NoteWithId, "user"> {
-    user: UserWithId;
+	user: UserWithId;
 }
 
 export interface NoteRequestBody extends Partial<Note> {
-    id: string;
+	id: string;
 }
 export interface NoteQueryResponse {
-    data: PopulatedNote[];
-    totalPages: number;
+	data: PopulatedNote[];
+	totalPages: number;
 }
 
 export interface SortBy {
-    [key: string]: SortOrder;
+	[key: string]: SortOrder;
 }
 
 export type TaskSort = "newest" | "oldest" | "pending" | "completed";
--- backend/src/utils/helpers/type_helpers.ts
+++ backend/src/utils/helpers/type_helpers.ts
@@ -2,83 +2,83 @@ import { UserRequestBody, Role, NoteRequestBody } from "../../types/types";
 import { Types } from "mongoose";
 
 export const isString = (param: unknown): param is string => {
-    return typeof param === "string" || param instanceof String;
+	return typeof param === "string" || param instanceof String;
 };
 
 const isRole = (value: unknown): value is Role => {
-    return ["admin", "manager", "employee"].includes(value as Role);
+	return ["admin", "manager", "employee"].includes(value as Role);
 };
 
 const isBoolean = (value: unknown): value is boolean => {
-    return typeof value === "boolean";
+	return typeof value === "boolean";
 };
 
 export const toUserRequestBody = (body: unknown): UserRequestBody => {
-    if (!body || typeof body !== "object")
-        throw new Error("Invalid or missing data");
+	if (!body || typeof body !== "object")
+		throw new Error("Invalid or missing data");
 
-    const { id, username, password, roles, active } = body as UserRequestBody;
+	const { id, username, password, roles, active } = body as UserRequestBody;
 
-    if (id !== undefined && !isString(id)) {
-        throw new Error("Invalid data: id should be a string");
-    }
+	if (id !== undefined && !isString(id)) {
+		throw new Error("Invalid data: id should be a string");
+	}
 
-    if (username !== undefined && !isString(username)) {
-        throw new Error("Invalid data: username should be a string");
-    }
+	if (username !== undefined && !isString(username)) {
+		throw new Error("Invalid data: username should be a string");
+	}
 
-    if (password !== undefined && !isString(password)) {
-        throw new Error("Invalid data: password should be a string");
-    }
+	if (password !== undefined && !isString(password)) {
+		throw new Error("Invalid data: password should be a string");
+	}
 
-    if (roles !== undefined) {
-        if (!Array.isArray(roles)) {
-            throw new Error("Invalid data: roles should be an array");
-        }
-        if (!roles.every(isRole)) {
-            throw new Error("Invalid data: roles contain invalid values");
-        }
-    }
+	if (roles !== undefined) {
+		if (!Array.isArray(roles)) {
+			throw new Error("Invalid data: roles should be an array");
+		}
+		if (!roles.every(isRole)) {
+			throw new Error("Invalid data: roles contain invalid values");
+		}
+	}
 
-    if (active !== undefined && !isBoolean(active)) {
-        throw new Error("Invalid data: active status should be a boolean");
-    }
+	if (active !== undefined && !isBoolean(active)) {
+		throw new Error("Invalid data: active status should be a boolean");
+	}
 
-    return {
-        id,
-        username,
-        password,
-        roles,
-        active,
-    };
+	return {
+		id,
+		username,
+		password,
+		roles,
+		active,
+	};
 };
 
 export const toNoteRequestBody = (body: unknown): NoteRequestBody => {
-    if (!body || typeof body !== "object")
-        throw new Error("Invalid or missing data");
+	if (!body || typeof body !== "object")
+		throw new Error("Invalid or missing data");
 
-    const { id, user, title, text, completed } = body as NoteRequestBody;
+	const { id, user, title, text, completed } = body as NoteRequestBody;
 
-    if (id !== undefined && !isString(id)) {
-        throw new Error("Invalid data: id should be a string");
-    }
-    if (user !== undefined && !Types.ObjectId.isValid(user)) {
-        throw new Error("Invalid data: user should be a string");
-    }
-    if (title !== undefined && !isString(title)) {
-        throw new Error("Invalid data: title should be a string");
-    }
-    if (text !== undefined && !isString(text)) {
-        throw new Error("Invalid data: text should be a string");
-    }
-    if (completed !== undefined && typeof completed !== "boolean") {
-        throw new Error("Invalid data: completed should be a boolean");
-    }
-    return {
-        id,
-        user,
-        title,
-        text,
-        completed,
-    };
+	if (id !== undefined && !isString(id)) {
+		throw new Error("Invalid data: id should be a string");
+	}
+	if (user !== undefined && !Types.ObjectId.isValid(user as Types.ObjectId)) {
+		throw new Error("Invalid data: user should be a string");
+	}
+	if (title !== undefined && !isString(title)) {
+		throw new Error("Invalid data: title should be a string");
+	}
+	if (text !== undefined && !isString(text)) {
+		throw new Error("Invalid data: text should be a string");
+	}
+	if (completed !== undefined && typeof completed !== "boolean") {
+		throw new Error("Invalid data: completed should be a boolean");
+	}
+	return {
+		id,
+		user,
+		title,
+		text,
+		completed,
+	};
 };
